7c99b1660b3cfb19cde776240cce30863cabf47f
mqnc <mirko.kunze@web.de>
2023-02-26 19:49:59 +0800

    fix demo

--- demo.sh
+++ demo.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
 
+mkdir -p demo
+cd demo
+
+PYEND="../src/pyend/pyend.py"
+
 echo removing all end marks...
-echo python3 pyend.py pyend.py -s --out pyend.noend.py
-python3 pyend.py pyend.py -s --out pyend.noend.py
+echo python3 $PYEND $PYEND -s --out pyend.noend.py
+python3 $PYEND $PYEND -s --out pyend.noend.py
 echo
 
 echo inserting end marks...
-echo python3 pyend.py pyend.py -e --out pyend.end.py
-python3 pyend.py pyend.py -e --out pyend.end.py
+echo python3 $PYEND pyend.noend.py -e --out pyend.end.py
+python3 $PYEND pyend.noend.py -e --out pyend.end.py
 echo
 
 echo comparing...
-echo diff -s pyend.end.py pyend.py
-diff -s pyend.end.py pyend.py
+echo diff -s pyend.end.py $PYEND
+diff -s pyend.end.py $PYEND
 echo
 
 echo removing all indentation...
-echo sed 's/	//g' pyend.py > pyend.noindent.txt
-sed 's/	//g' pyend.py > pyend.noindent.txt
+echo "sed 's/	//g' $PYEND > pyend.noindent.txt"
+sed 's/	//g' $PYEND > pyend.noindent.txt
 echo
 
 echo restoring indentation...
-echo python3 pyend.py pyend.noindent.txt -i --out pyend.reindent.py
-python3 pyend.py pyend.noindent.txt -i --out pyend.reindent.py
+echo python3 $PYEND pyend.noindent.txt -i --out pyend.reindent.py
+python3 $PYEND pyend.noindent.txt -i --out pyend.reindent.py
 echo
 
 echo comparing...
-echo diff -s pyend.py pyend.reindent.py
-diff -s pyend.py pyend.reindent.py
+echo diff -s $PYEND pyend.reindent.py
+diff -s $PYEND pyend.reindent.py
 echo
